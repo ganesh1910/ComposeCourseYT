3c5f740e57a21620b2272419b242a9c1bb15c09a
Ganesh Khandagale <68702026+ganesh1910@users.noreply.github.com>
2022-11-21 19:01:43 +0530

    Modifiers - commits

--- app/src/main/java/com/gktech/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/gktech/composecourseyt/MainActivity.kt
@@ -4,33 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.gktech.composecourseyt.ui.theme.ComposeCourseYTTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(
+            Column(
                 modifier = Modifier
-                    .width(300.dp)
-                    .fillMaxHeight(0.7f)
-                    .background(Color.Green),
-                horizontalArrangement = Arrangement.SpaceEvenly,
-                verticalAlignment = Alignment.CenterVertically
+                    .fillMaxHeight(0.5f)
+                    .background(Color.Green)
+                    .fillMaxWidth()
+                    .border(5.dp, Color.Magenta)
+                    .padding(5.dp)
+                    .border(5.dp, Color.Blue)
+                    .padding(5.dp)
+                    .border(10.dp, Color.Red)
+                    .padding(10.dp)
+//                    .width(600.dp)
+//                    .requiredWidth(600.dp)
             ) {
                 Text("Hello")
+                Spacer(modifier = Modifier.height(50.dp))
                 Text("World")
-                Text("Welcome")
             }
         }
     }
