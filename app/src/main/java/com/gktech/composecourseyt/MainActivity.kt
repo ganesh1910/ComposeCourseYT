ff0d6c30e4acedc8853e9a1d182d274c33dd679d
Ganesh Khandagale <68702026+ganesh1910@users.noreply.github.com>
2022-11-25 16:46:40 +0530

    list

--- app/src/main/java/com/gktech/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/gktech/composecourseyt/MainActivity.kt
@@ -3,22 +3,14 @@ package com.gktech.composecourseyt
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -26,60 +18,47 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val painter = painterResource(R.drawable.kermit)
-            val description = "kermit playing in the snow"
-            val title = "kermit playing in the snow"
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth(0.5f)
-                    .padding(16.dp)
-            ) {
-                ImageCard(painter = painter, contentDescription = description, title = title)
-            }
-        }
-    }
-}
 
-@Composable
-fun ImageCard(
-    painter: Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier = Modifier
-) {
-    Card(
-        modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(15.dp),
-        elevation = 5.dp
-    ) {
-        Box(modifier = Modifier.height(200.dp)) {
-            Image(
-                painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Crop
-            )
-            Box(
-                modifier =  Modifier
-                    .fillMaxSize()
-                    .background(
-                        Brush.verticalGradient(
-                            colors = listOf(
-                                Color.Transparent,
-                                Color.Black
-                            ),
-                            startY = 300f
-                        )
+            LazyColumn {
+                itemsIndexed(
+                    listOf("This", "is", "the", "Compose","A","B","C","D","E","F","G","H","I","J","K")
+                ) { _, item ->
+                    Text(
+                        text = "item $item",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 24.dp)
                     )
-            )
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(12.dp),
-                contentAlignment = Alignment.BottomStart
-            ) {
-                Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+                }
+
+                /* items(500) {
+                     Text(
+                         text = "item $it",
+                         fontSize = 24.sp,
+                         fontWeight = FontWeight.Bold,
+                         textAlign = TextAlign.Center,
+                         modifier = Modifier
+                             .fillMaxWidth()
+                             .padding(vertical = 24.dp)
+                     )
+                 }*/
+                /*  val scrollState = rememberScrollState()
+                  Column(modifier = Modifier.verticalScroll(scrollState)) {
+                      for (i in 1..50) {
+                          Text(
+                              text = "item $i",
+                              fontSize = 24.sp,
+                              fontWeight = FontWeight.Bold,
+                              textAlign = TextAlign.Center,
+                              modifier = Modifier
+                                  .fillMaxWidth()
+                                  .padding(vertical = 24.dp)
+                          )
+                      }*/
             }
         }
     }
-
 }
